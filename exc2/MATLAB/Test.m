5312d994acb65a9202d6972e2c6354f9551f3091
Sifan Jiang <jasper.sifan@gmail.com>
2019-09-07 00:19:35 +0200

    Readme to go

--- exc2/MATLAB/Test.m
+++
@@ -1,5 +0,0 @@
-close all; clear; clc;
-
-t = 1:0.01:10;
-h = 3 .* pi .* exp(- 5.*sin(2.*pi.*1.*t));
-plot(t,h)
--- exc2/kthfsdv_exc2.py
+++ exc2/kthfsdv_exc2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import matplotlib
+# import matplotlib2tikz
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -13,12 +14,13 @@ import pandas as pd
 import threading
 import Tkinter as tk
 import ttk
+# from tkinter import ttk
 
 
 class exc2():
     T = 1.0
     N = 100
-    NAME = ""
+    NAME = "Experiment_1"
 
     def __init__(self):
         self.ts = []
@@ -26,7 +28,6 @@ class exc2():
         self.dt = self.T / self.N
         self.t  = 0
         self.STATE = False
-        print("Class Created", self.T, self.N, self.ts, self.hs, self.dt, self.t)
 
     def _update_(self):
         self.ts.append(self.t)
@@ -52,59 +53,75 @@ class exc2():
                 axl.scatter(self.ts, self.hs, marker=".")
                 canvas.draw()
 
-        def name(Name):
-            self.Name = Name
+        def name(entry):
+            self.NAME = entry.get()
+            print(self.NAME)
+
+        def tex():
+            # matplotlib2tikz.save("File.tex")
+            # FILENAME = self.NAME + ".tex"
+            print("Not supported.")
 
         def start():
-            self.STATE = True
-            threading.Thread(target=animate).start()
+            if not self.STATE:
+                self.STATE = True
+                threading.Thread(target=animate).start()
 
         def stop():
-            self.STATE = False
-            threading.Thread(target=animate).start()
+            if self.STATE:
+                self.STATE = False
+                threading.Thread(target=animate).start()
 
         def reset():
             self.STATE = False
             self.__init__()
-            threading.Thread(target=animate).start()
+            axl.clear()
+            axl.set_xlabel("Time")
+            axl.set_ylabel("Value")
+            axl.scatter(self.ts, self.hs, marker=".")
+            canvas.draw()
 
         def save():
             df = pd.DataFrame({'Time':self.ts, 'H':self.hs})
-            df.to_csv("data.csv")
+            df.to_csv(self.NAME+".csv")
+            print("Data saved.")
 
 
-        fig = Figure(figsize=(8, 8), dpi=100)
+        fig = Figure(figsize=(6, 6), dpi=100)
         axl = fig.add_subplot(111)
         axl.set_xlabel("Time")
         axl.set_ylabel("Value")
         # axl.grid()
 
         root = tk.Tk()
-        root.minsize(800, 800)
+        # root.minsize(600, 600)
         root.wm_title("KTHFS: Data Visualization")
         # root.iconbitmap("DV.ico")
 
         experiment_name = ttk.Entry(root)
-        experiment_name.grid(row=0, columnspan=3)
+        experiment_name.grid(row=0, column=1)
 
-        name_button = ttk.Button(root, text="Enter Experiment Name")
-        name_button.grid(row=0, column=3)
+        name_button = ttk.Button(root, text="Change Experiment Name", command=lambda:name(experiment_name))
+        name_button.grid(row=0, column=2)
+
+        tex_button = ttk.Button(root, text="Save as Tikz", command=tex)
+        tex_button.grid(row=0, column=3)
 
         start_button = ttk.Button(root, text="Start", command=start)
-        start_button.grid(row=0, column=0)
+        start_button.grid(row=1, column=0)
 
         stop_button = ttk.Button(root, text="Stop", command=stop)
-        stop_button.grid(row=0, column=1)
+        stop_button.grid(row=1, column=1)
 
         reset_button = ttk.Button(root, text="Reset", command=reset)
-        reset_button.grid(row=0, column=2)
+        reset_button.grid(row=1, column=2)
 
-        save_button = ttk.Button(root, text="Save") #, command=save)
-        save_button.grid(row=0, column=3)
+        save_button = ttk.Button(root, text="Save as CSV", command=save)
+        save_button.grid(row=1, column=3)
 
         canvas = FigureCanvasTkAgg(fig, master=root)
         canvas.draw()
-        canvas.get_tk_widget().grid(row=1, columnspan=4)
+        canvas.get_tk_widget().grid(row=2, columnspan=4)
         # canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         # toolbar = NavigationToolbar2Tk(canvas, root)
